3718a879cd34c3b22f0bd390e510260dc413fa68
葉智翔 <yehchihhsiang@yezhixiangdeMacBook-Pro.local>
2021-06-10 18:34:52 +0800

    New

--- Code_for_Analysis/A_Test_Test_Compile.C
+++ Code_for_Analysis/A_Test_Test_Compile.C
@@ -6,5 +6,19 @@ void A_Test_Test_Compile()
     vector<double> Position2={10,5,8};
     cout << "Test3: " << Test3(1) << endl;
     cout << "The_smallest_in_the_vector(Position2): " << The_smallest_in_the_vector(Position2) << endl;
+    
+    gRandom = new TRandom3(0);
+    gRandom->SetSeed(0);
+
+    TF1 *Two_Sides    = new TF1("Two_Sides","x",0,2);
+    double Side_confirmed = Two_Sides->GetRandom();//Give out 0,1;
+    cout << "Side_confirmed: " << Side_confirmed << endl;
+    
+    int A=0;
+    int B=0;
+    if(A==0) B=1;
+    if(B==1) cout << "ok: " << endl;
+
     return 0;
+
 }
--- Code_for_Analysis/dsigma_dT2_Bent_TEXONO.h
+++ Code_for_Analysis/dsigma_dT2_Bent_TEXONO.h
@@ -30,15 +30,29 @@ double Vector_for_axis[6][3]=
     0,0,-1,
 }
 
-double H_OCT_KS    =70.;//m, High_Outer_Ceiling_Top_KS: 70m
-double H_ICT_KS    =60.;//m, High_inner_Ceiling_Top_KS: 60m
+double RP = 0.5;//Reference_Point
+double H_OCT_KS    =70.-RP;//m, High_Outer_Ceiling_Top_KS: 70m
+double H_ICT_KS    =60.-RP;//m, High_inner_Ceiling_Top_KS: 60m
 
 double R_OW_KS     =57.;//m, Radius_Outer_Wall_KS: 57m
 double R_IW_KS     =56.;//m, Radius_Inner_Wall_KS: 56m
 
-double H_RE        =50.;//m, High_Reactor: 50m
+double H_RE        =50.-RP;//m, High_Reactor: 50m
 double R_OWALL_RE  =28.;//m, Radius_Outer_Wall_Reactor: 28m
 double R_IWATER_RE =27.;//m, Radius_Inner_Water_Reactor: 27m
+
+double The_smallest_in_a_vector(vector<double> Vector)
+{
+    double Return_the_smallest_one=0;
+    if(Vector.size()>0)
+    {
+        std::sort(Vector.begin(), Vector.end());
+        Return_the_smallest_one = Vector[0];
+    }
+    else{Return_the_smallest_one=0;}
+    
+    return Return_the_smallest_one;
+}
 
 //Starting Position from the surface of the detector
 double *Starting_Position()
@@ -47,37 +61,24 @@ double *Starting_Position()
     double SPA[3];//Starting_Positions
     double VR[2];//Vector_Restriction[0]:Coordinate Vector_Restriction[1]:Plus or Minus
     
-    TF1 *X_Detector = new TF1("X_Detector","X_Detector",-Ge_Layer[0]*0.5,Ge_Layer[0]*0.5);
-    TF1 *Y_Detector = new TF1("Y_Detector","Y_Detector",-Ge_Layer[1]*0.5,Ge_Layer[1]*0.5);
-    TF1 *Z_Detector = new TF1("Z_Detector","Z_Detector",-Ge_Layer[2]*0.5,Ge_Layer[2]*0.5);
+    TF1 *X_Detector = new TF1("X_Detector","x",-Ge_Layer[0]*0.5,Ge_Layer[0]*0.5);
+    TF1 *Y_Detector = new TF1("Y_Detector","x",-Ge_Layer[1]*0.5,Ge_Layer[1]*0.5);
+    TF1 *Z_Detector = new TF1("Z_Detector","x",-Ge_Layer[2]*0.5,Ge_Layer[2]*0.5);
 
     gRandom = new TRandom3(0);
     gRandom->SetSeed(0);
-    //Choose the plane which the WIMP runs from
-    TF1 *X_Y_Z_Choose = new TF1("X_Y_Z_Choose","X_Y_Z_Choose",0,3);
-    int Plane_Chosen= X_Y_Z_Choose->GetRandom();
+    //Choose the plane where the WIMP starts running
+    TF1 *X_Y_Z_Choose = new TF1("X_Y_Z_Choose","x",0,3);
+    int Plane_Chosen= X_Y_Z_Choose->GetRandom();//Give out 0,1,2;
+    //Two sides for WIMPs
+    TF1 *Two_Sides    = new TF1("Two_Sides","x",0,2);
+    int Side_confirmed = Two_Sides->GetRandom();//Give out 0,1;
+    double Confirmed_Side[2]={1,-1};
+    //Fix the point on a certain plane
+    SPA[Plane_Chosen]= Confirmed_Side[Side_confirmed]*Ge_Layer[Plane_Chosen]*0.5;VR[0]=Plane_Chosen;VR[1]=Confirmed_Side[Side_confirmed];
+    //Decide the rest of the coordination
+    if(SPA[0]==0)SPA[0]=X_Detector->GetRandom();if(SPA[1]==0)SPA[1]=Y_Detector->GetRandom();if(SPA[2]==0)SPA[2]=Z_Detector->GetRandom();
 
-    TF1 *Two_Sides    = new TF1("Two_Sides","Two_Sides",0,2);
-    int Side_confirmed = Two_Sides->GetRandom();
-
-    if(Random_Value==0)//On X-Plane
-    {
-        if(Side_confirmed==0){SPA[0]=-Ge_Layer[0]*0.5;VR[0]=1;VR[1]=-1;}
-        if(Side_confirmed==1){SPA[0]= Ge_Layer[0]*0.5;VR[0]=1;VR[1]= 1;}
-        SPA[1]=Y_Detector->GetRandom();SPA[2]=Z_Detector->GetRandom();
-    }
-    if(Random_Value==1)//On Y-Plane
-    {
-        if(Side_confirmed==0){SPA[1]=-Ge_Layer[1]*0.5;VR[0]=2;VR[1]=-1;}
-        if(Side_confirmed==1){SPA[1]= Ge_Layer[1]*0.5;VR[0]=2;VR[1]= 1;}
-        SPA[0]=X_Detector->GetRandom();SPA[2]=Z_Detector->GetRandom();
-    }
-    if(Random_Value==2)//On Z-Plane
-    {
-        if(Side_confirmed==0){SPA[2]=-Ge_Layer[2]*0.5;VR[0]=3;VR[1]=-1;}
-        if(Side_confirmed==1){SPA[2]= Ge_Layer[2]*0.5;VR[0]=3;VR[1]= 1;}
-        SPA[0]=X_Detector->GetRandom();SPA[1]=Y_Detector->GetRandom();
-    }
     Return_Value[0]=SPA[0];Return_Value[1]=SPA[1];Return_Value[2]=SPA[2];
     Return_Value[3]= VR[0];Return_Value[4]= VR[1];
     return(Return_Value);
@@ -124,6 +125,34 @@ int *Where_is_it_in_shielding(double *Position)
 }
 
 
+double Length_to_six_planes(int Layer, double *POS, double *DR)//Position(POS),
+{
+    double POS_Aft[3];
+    double ET_XYZ[3][2];//Extended_Times_XYZ
+    vector<double> Find_smallest_Vector;
+    double The_final_scaling;
+    double Confirmed_Side[2]={1,-1};
+
+    // Discover the length to six places
+    for(int XYZ=0; XYZ<3; XYZ++)//The Axis
+    {
+        for(int PorN=0; PorN<2; PorN++)//Positive Or Negative
+        {
+            double ET = (Confirmed_Side[PorN]*The_Material_Layer[Layer][XYZ]-POS[XYZ])/(DR[XYZ]);//Extension_times
+            for(int POS_After_Axis=0; POS_After_Axis<3; POS_After_Axis++){POS_Aft[POS_After_Axis] = POS[POS_After_Axis] + ET*DR[POS_After_Axis];}
+            double *Fun_Layer_Aft= Where_is_it_in_shielding(POS_Aft);
+            if(Fun_Layer_Aft[0]==Layer){ET_XYZ[XYZ][PorN]=ET;}else{ET_XYZ[XYZ][PorN]=0;}
+            if(Fun_Layer_Aft[0]==Layer and ET>0){Find_smallest_Vector.push_back(ET);}
+        }
+    }
+    
+    The_final_scaling = The_smallest_in_a_vector(Find_smallest_Vector);
+    //The smallest one
+    Find_smallest_Vector.clear();
+    
+    return The_final_scaling;
+
+}
 
 int DR_In_or_Out_on_surface(int IOS, double *DR)//Index_of_surface(IOS),Direction(DR)
 {//For the shielding
@@ -145,80 +174,6 @@ int DR_In_or_Out_off_surface(double *POS, double *DR)
     return(In_or_Out_N);
 }
 
-
-double The_smallest_in_a_vector(vector<double> Vector)
-{
-    double Return_the_smallest_one=0;
-    if(Vector.size()>0)
-    {
-        std::sort(Vector.begin(), Vector.end());
-        Return_the_smallest_one = Vector[0];
-    }
-    else{Return_the_smallest_one=0;}
-    
-    return Return_the_smallest_one;
-}
-
-double Length_to_six_planes(int Layer, double *POS, double *DR)//Position(POS),
-{
-    double POS_Aft[3];
-    double ET_XYZ[3][2];//Extended_Times_XYZ
-    vector<double> Find_smallest_Vector;
-    double The_final_scaling;
-    // Discover the length to six places
-    for(int XYZ=0; XYZ<3; XYZ++)//The Axis
-    {
-        for(int PorN=0; PorN<2; PorN++)//Positive Or Negative
-        {
-            double ET = (The_Material_Layer[Layer][XYZ]-POS[XYZ])/(DR[XYZ]);//Extension_times
-            for(int POS_After_Axis=0; POS_After_Axis<3; POS_After_Axis++){POS_Aft[POS_After_Axis] = POS[POS_After_Axis] + ET*DR[POS_After_Axis];}
-            double *Fun_Layer_Aft= Where_is_it_in_shielding(POS_Aft);
-            if(Fun_Layer_Aft[0]==Layer){ET_XYZ[XYZ][PorN]=ET;}else{ET_XYZ[XYZ][PorN]=0;}
-            if(Fun_Layer_Aft[0]==Layer and ET_XYZ[XYZ][PorN]>0){Find_smallest_Vector.push_back(ET_XYZ[XYZ][PorN]);}
-        }
-    }
-    
-    The_final_scaling = The_smallest_in_a_vector(Find_smallest_Vector);
-    //The smallest one
-    Find_smallest_Vector.clear();
-    
-    return The_final_scaling;
-
-}
-
-double SFLE(double a, double b, double c) // Solution_for_Linear_Equation[a(X^2) + b(X) + c = 0]
-{//Check OK!
-    double Denominator=0; //Down
-    double Numerator1=0;double Numerator2=0;double Numerator_Final=0; //Up
-    vector<double> Find_smallest_Vector;
-    
-    Denominator = 2*a;
-    Numerator1 = -b + sqrt(b*b-4*a*c);
-    if(Numerator1>0)Find_smallest_Vector.push_back(Numerator1);
-    Numerator2 = -b - sqrt(b*b-4*a*c);
-    if(Numerator2>0)Find_smallest_Vector.push_back(Numerator2);
-    
-    Numerator_Final = The_smallest_in_a_vector(Find_smallest_Vector);
-
-    return (Numerator_Final/Denominator);
-}
-
-double Scaling_to_others_XY(double *POS, double *DR, double R)//Position(POS),Direction(DR),Radius(R)
-{
-    return SFLE( (DR[0]*DR[0]+DR[1]*DR[1]),(2*POS[0]*DR[0]+2*(POS[1]-30)*DR[1]),(POS[0]*POS[0]+(POS[1]-30)*(POS[1]-30)-R*R) );
-}
-double Scaling_to_others_Z(double *POS, double *DR, double H)//Position(POS),Direction(DR),Radius(R)
-{
-    return (H-POS[2])/DR[2];
-}
-double Radius_for_ceiling(double *POS, double *DR, double S)//Position(POS),Direction(DR),Scaling(S),Radius(R)
-{
-    double X = POS[0]+DR[0]*S;
-    double Y = (POS[1]-30)+DR[1]*S;
-    return sqrt(X*X+Y*Y);
-}
-
-
 double CID(double *POS, double *DR)//Criteria_In_Shielding(CID),Position(POS),Direction(DR)
 {
     double RSF;//Return_Scaling_Factor
@@ -228,11 +183,6 @@ double CID(double *POS, double *DR)//Criteria_In_Shielding(CID),Position(POS),Di
     int  A_Surface_index  = A_Position[1];
     int  DR_IN_OR_OUT = 0;
     
-    double Shielding_Scaling;double Down_floor_Scaling;
-    double Top_ceiling_Scaling;double Low_ceiling_Scaling;
-    double Outer_Wall_Scaling;double Inner_Wall_Scaling;
-    double Outer_RE_Scaling;double Inner_RE_Scaling;
-
     //In the shielding
     if( (A_Layer<4) )
     {
@@ -265,13 +215,6 @@ double CID(double *POS, double *DR)//Criteria_In_Shielding(CID),Position(POS),Di
             {
                RSF = std::min(Length_to_six_planes(A_Layer,POS,DR),Length_to_six_planes(A_Layer-1,POS,DR))
             }
-            if(DR_IN_OR_OUT==1)
-            {
-                vector<double>Find_smallest_Vector;
-                Inner_Wall_Scaling = Scaling_to_others_XY(POS,DR,R_IW_KS);
-                if(POS[2]+Inner_Wall_Scaling*DR[2]<=H_ICT_KS)Find_smallest_Vector.push_back(Inner_Wall_Scaling);
-
-            }
         }
         if(A_Surface_index==0)//Off the surface
         {
@@ -284,9 +227,46 @@ double CID(double *POS, double *DR)//Criteria_In_Shielding(CID),Position(POS),Di
         }
 
     }
-        return RSF;
+        
+    return RSF;
 }
 
+
+
+
+
+
+double SFLE(double a, double b, double c) // Solution_for_Linear_Equation[a(X^2) + b(X) + c = 0]
+{//Check OK!
+    double Denominator=0; //Down
+    double Numerator1=0;double Numerator2=0;double Numerator_Final=0; //Up
+    vector<double> Find_smallest_Vector;
+    
+    Denominator = 2*a;
+    Numerator1 = -b + sqrt(b*b-4*a*c);
+    if(Numerator1>0)Find_smallest_Vector.push_back(Numerator1);
+    Numerator2 = -b - sqrt(b*b-4*a*c);
+    if(Numerator2>0)Find_smallest_Vector.push_back(Numerator2);
+    
+    Numerator_Final = The_smallest_in_a_vector(Find_smallest_Vector);
+
+    return (Numerator_Final/Denominator);
+}
+
+double Scaling_to_others_XY(double *POS, double *DR, double R)//Position(POS),Direction(DR),Radius(R)
+{
+    return SFLE( (DR[0]*DR[0]+DR[1]*DR[1]),(2*POS[0]*DR[0]+2*(POS[1]-30)*DR[1]),(POS[0]*POS[0]+(POS[1]-30)*(POS[1]-30)-R*R) );
+}
+double Scaling_to_others_Z(double *POS, double *DR, double H)//Position(POS),Direction(DR),Radius(R)
+{
+    return (H-POS[2])/DR[2];
+}
+double Radius_for_ceiling(double *POS, double *DR, double S)//Position(POS),Direction(DR),Scaling(S),Radius(R)
+{
+    double X = POS[0]+DR[0]*S;
+    double Y = (POS[1]-30)+DR[1]*S;
+    return sqrt(X*X+Y*Y);
+}
 double RTC(double *POS)//Radius_To_Center(RTC) on XY plane
 {
     double X = (POS[0]-0);
@@ -302,12 +282,12 @@ int *Where_is_it_out_of_shielding(double *POS)
     if( (RTC(POS)>R_IWATER_RE) and (RTC(POS)<=R_OWALL_RE) and (POS[2]<=H_RE) )//Outer Wall of Reactor
     {
         Component==1;
-        if(RTC(POS)==R_OWALL_RE)On_Surface==2;
+        if(POS[2]==H_RE)On_Surface==2;if(RTC(POS)==R_OWALL_RE)On_Surface==2;
     }
     if( (RTC(POS)<=R_IWATER_RE) and (POS[2]<=H_RE) )//Inner Wall of Reactor
     {
         Component==2;
-        if(RTC(POS)==R_IWATER_RE)On_Surface==2;
+        if(POS[2]==H_RE)On_Surface==1;if(RTC(POS)==R_IWATER_RE)On_Surface==2;
     }
     if( (RTC(POS)<=R_OW_KS) and (RTC(POS)>=R_IW_KS) and (POS[2]<=H_ICT_KS) )//Side Wall of KS
     {
@@ -322,7 +302,7 @@ int *Where_is_it_out_of_shielding(double *POS)
     if( (RTC(POS)>R_IW_KS) and (RTC(POS)<=R_OW_KS) and (POS[2]>H_ICT_KS) and (POS[2]<=H_OCT_KS))//Corner of the wall
     {
         component==5;
-        if( (RTC(POS)==R_OW_KS) or (POS[2]==H_OCT_KS) )On_Surface==2;
+        if(RTC(POS)==R_OW_KS)On_Surface==2;if(POS[2]==H_OCT_KS)On_Surface==22;
     }
     Return_Value[0]=component;Return_Value[1]=On_Surface;
     
@@ -347,7 +327,7 @@ double DOLOUD(double *Component)//Decision_On_Length_Out_Of_Detector(DOLOUD)
     }
     if(Component[3]==1){//Inner_Reactor
         double Inner_RE_Scaling = Scaling_to_others_XY(POS,DR,R_IWATER_RE);
-        if(POS[2]+Outer_RE_Scaling*DR[2]<=H_RE)Find_smallest_Vector.push_back(Inner_RE_Scaling);
+        if(POS[2]+Inner_RE_Scaling*DR[2]<=H_RE)Find_smallest_Vector.push_back(Inner_RE_Scaling);
     }
     if(Component[4]==1){//Inner_Wall
         double Inner_Wall_Scaling = Scaling_to_others_XY(POS,DR,R_IW_KS);
@@ -390,14 +370,28 @@ double COFD(double *POS, double *DR)//Criteria_out_of_Shielding(COFD),Position(P
     
     double Scaling_Length;
     
-    if(A_Layer==4 and A_Surface_index!=0){double Arrival_of_point[7]={1,1,1,0,1,0,1,0};Scaling_Length = DOLOUD(Arrival_of_point);}
+    if(A_Layer==4 and A_Surface_index!=0){double Arrival_of_point[8]={1,1,1,0,1,0,1,0};Scaling_Length = DOLOUD(Arrival_of_point);}
     if(Compoent==1)//
     {
+        if(A_Surface_index==1)
+        {
+            if(DR[2]>=0)
+            {
+                double Arrival_of_point[8]={0,0,0,0,1,0,1,0};
+                Scaling_Length = DOLOUD(Arrival_of_point);
+            }
+            if(DR[2]<0)
+            {
+                double Arrival_of_point[8]={1,0,1,1,0,0,0,0};
+                Scaling_Length = DOLOUD(Arrival_of_point);
+            }
+        }
+
         if(A_Surface_index==2)
         {
             if(DIOOTC(POS,DR)>=0)
             {
-                double Arrival_of_point[8]={1,1,1,0,1,0,1,0};
+                double Arrival_of_point[8]={1,1,0,0,1,0,1,0};
                 Scaling_Length = DOLOUD(Arrival_of_point);
             }
         }
@@ -446,18 +440,31 @@ double COFD(double *POS, double *DR)//Criteria_out_of_Shielding(COFD),Position(P
     {
         if( (A_Surface_index==1 and DR[2]<0) )
         {
-                double Arrival_of_point[8]={1,0,0,0,0,1,0,1};
+                double Arrival_of_point[8]={1,1,1,1,1,0,0,0};
                 Scaling_Length = DOLOUD(Arrival_of_point);
         }
-        if( (A_Surface_index==2 and DIOOTC(POS,DR)<0) )
+        if( (A_Surface_index==0 and DR[2]>=0) )
         {
-                double Arrival_of_point[8]={1,0,0,0,1,0,0,1};
+                double Arrival_of_point[8]={0,0,0,0,0,1,0,1};
                 Scaling_Length = DOLOUD(Arrival_of_point);
         }
-        if( (A_Surface_index==0) )
+        if( (A_Surface_index==2) and DR[2]<0 )
         {
-                double Arrival_of_point[8]={1,0,0,0,1,1,0,1};
+                double Arrival_of_point[8]={0,0,0,0,0,1,1,0};
                 Scaling_Length = DOLOUD(Arrival_of_point);
+        }
+    }
+    if(Compoent==5)//
+    {
+        if( (A_Surface_index==2) and DIOOTC(POS,DR)<0 )
+        {
+            double Arrival_of_point[8]={0,0,0,0,1,0,1,1};
+            Scaling_Length = DOLOUD(Arrival_of_point);
+        }
+        if( (A_Surface_index==22) and DR[2]<0 )
+        {
+            double Arrival_of_point[8]={0,0,0,0,1,1,1,0};
+            Scaling_Length = DOLOUD(Arrival_of_point);
         }
     }
 
